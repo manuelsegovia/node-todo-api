3eae450ae3b2d909784748d0a281b8c9a9dd73d8
Manuel Segovia <msegovia@mac.com>
2019-09-14 19:56:48 -0500

    with PATCH route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ =require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -21,6 +22,7 @@ app.post('/todos',(req,res)=>{
     res.status(400).send(e);
   });
 });
+// GET todos
 app.get('/todos',(req, res)=>{
   Todo.find().then((todos)=>{
     res.send({todos});
@@ -29,7 +31,7 @@ app.get('/todos',(req, res)=>{
   });
 });
 
-//GET todos
+//GET todo
 app.get('/todos/:id', (req,res)=>{
   let id =req.params.id;
   if(!ObjectID.isValid(id)){
@@ -64,6 +66,31 @@ app.delete('/todos/:id', (req, res)=>{
 
 app.listen(port, ()=>{
   console.log(`Started on port ${port}`);
+});
+
+// PATCH todo
+app.patch('/todos/:id', (req,res)=>{
+  let id = req.params.id;
+  let body = _.pick(req.body,['text','completed']);
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+
+
 });
 
 module.exports = {app};
